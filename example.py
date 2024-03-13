6265701748e3202dc9b51082d49f6fe013d59770
Justin <justlee888@gmail.com>
2024-03-12 18:07:30 -0700

    reviews only after 2015

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ import time
 reviews=[]
 
 start = time.time()
-with open("users_rows.csv") as prof_rows:
+with open("test.csv") as prof_rows:
     reader = csv.DictReader(prof_rows)
     for row in reader:
         professor = get_professor_by_school_and_name(
@@ -13,23 +13,23 @@ with open("users_rows.csv") as prof_rows:
         if professor is not None and professor.school.name == "San Jose State University":
             ratings = professor.get_ratings()
             for rating in ratings:
-                class_name = rating.class_name
-                department,course_number = None,None
-                if "-" in class_name:
-                    split = class_name.split("-")
-                    department = split[0]
-                    course_number = split[1]
-                else:
-                    match = re.match(r"([A-Za-z]+)(\d+.*)", class_name)
-                    if match:
-                        department = match.group(1)
-                        course_number = match.group(2)
+                year = rating.date.year
+                if year >= 2015:
+                    class_name = rating.class_name
+                    department,course_number = None,None
+                    if "-" in class_name:
+                        split = class_name.split("-")
+                        department = split[0]
+                        course_number = split[1]
                     else:
-                        continue
-                reviews.append([1,row["id"],course_number,department,rating.comment,rating.rating,rating.difficulty,rating.grade,rating.rating_tags,rating.take_again,False])
-                # print(f"Date: {rating.date}, Rating: {rating.rating}, Content: {rating.comment}, Diffifculty:{rating.difficulty}, Grade: {rating.grade}, Take Again: {rating.take_again},  ")
-
-
+                        match = re.match(r"([A-Za-z]+)(\d+.*)", class_name)
+                        if match:
+                            department = match.group(1)
+                            course_number = match.group(2)
+                        else:
+                            continue
+                    reviews.append([1,row["id"],course_number,department,rating.comment,rating.rating,rating.difficulty,rating.grade,rating.rating_tags,rating.take_again,False])
+                    #print(f"Date: {rating.date}, Rating: {rating.rating}, Content: {rating.comment}, Diffifculty:{rating.difficulty}, Grade: {rating.grade}, Take Again: {rating.take_again},  ")
 
 def save_to_csv(filename, data):
     with open(filename, 'w', encoding="utf-8", newline='') as file:
@@ -37,6 +37,6 @@ def save_to_csv(filename, data):
         writer.writerow(["user_id","professor_id","course_number","department","content","quality","difficulty","grade","tags","take_again","is_user_anonymous"])
         writer.writerows(data)
 
-save_to_csv('reviews.csv',reviews)
+save_to_csv('test file.csv',reviews)
 end = time.time()
 print("took " + str(end - start) + " seconds")
